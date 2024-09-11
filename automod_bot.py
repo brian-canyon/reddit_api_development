64a463aff98e91ec575e6edead871655d89b28b9
brian-canyon <Canyon_Brian@bah.com>
2024-09-11 14:29:00 -0700

    Create automod_bot.py
    Developing a script to act as a reddit 'bot' and search for toxic words throughout various subreddits. This is very early in development

---
+++ automod_bot.py
@@ -0,0 +1,36 @@
+import requests
+import urllib3
+urllib3.disable_warnings() ## Having trouble with SSL, below code not suited for production
+
+def get_login_credentials():
+    login_creds = open("C:\\Users\\630227\\Documents\\redditlogin.txt","r")
+    login_dict = {}
+    for item in login_creds.readlines():
+        login_dict[item.split(" ")[0]] = item.split("'")[1]        
+    login_creds.close()
+    return login_dict      
+
+
+def get_reddit_token():
+    login_dict = get_login_credentials()
+    auth = requests.auth.HTTPBasicAuth(login_dict['personal_use_script'], login_dict['secret'])
+    data = {'grant_type': 'password',
+            'username': login_dict['username'],
+            'password': login_dict['password']}
+
+    headers = {'User-Agent': 'MyBot/0.0.1'}
+
+    res = requests.post('https://www.reddit.com/api/v1/access_token',
+                        auth=auth, data=data, headers=headers, verify = False)
+
+    TOKEN = res.json()['access_token']
+    headers = {**headers, **{'Authorization': f"bearer {TOKEN}"}}
+
+    return headers
+
+## Creating a function to pull all comments from a speicific sub, in the last 24 hours
+subreddit = "News"
+url = f"https://oauth.reddit.com/r/{subreddit}/hot"
+response = requests.get(url, headers=get_login_credentials(), verify= False)
+for post in response.json()['data']['children']:
+    print(post['data']['title'])
